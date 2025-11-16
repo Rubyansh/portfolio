1bf91f44ca84534a9cbe7e7fe70597a04f363c30
Rubyansh <141521530+Rubyansh@users.noreply.github.com>
2025-11-17 02:28:14 +0530

    Better metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,23 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Rubyansh - Full Stack Developer',
-  description: 'Full-Stack Web Developer, Software Developer, Game Developer, and CS Student',
+  title: 'Rubyansh',
+  description: 'Personal Portfolio',
+  robots: {
+    index: false,
+    follow: false,
+    noimageindex: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +32,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="robots" content="noindex, nofollow, noimageindex" />
+        <meta name="theme-color" content="#6E40C9" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
